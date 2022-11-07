d3f841e43fb5b03f67b236dc6160483f0ad44837
Jpdezoito <jpdezoito@gmail.com>
2022-11-07 13:35:15 -0300

    modifi

--- exercipodesubir/podesubir.js
+++ exercipodesubir/podesubir.js
@@ -1,18 +1,26 @@
-function podesubir(n1, n2) {  
-    let Tru = 'True'
-    let Fal = 'False'
-    console.log(n2 == 'Acompanhada' ? Tru : Fal)
-    let auto = 'Acesso autorizado'
-    let some = 'Acesso autorizado somente com acompanhante'    
-    if (n1 >= 1.40 && n1 <= 2.00) {                                   
-        return auto;
-    }else if (n1 < 1.40 && n1 >= 1.20, Fal ) {
-        return some 
-    }   
-    else if (n1 < 1.20) {
-        return 'Acesso negado'
+
+function podesubir(n1, n2) {
+    let acompanhado = (n2 == 'acompanhado'? 'true': 'false')
+    let naoacompanhado = (n2 == 'nao acompanhado'? 'false': 'true')    
+    console.log(acompanhado)
+    console.log(naoacompanhado) 
+     if (n1 >= 1.40 && n1 <= 2.00) {
+        return 'Acesso autorizado'
+     }else if (n1 = 1.39 && n1 >= 1.20){
+        console.log(n2 == 'acompanhado'? 'Acesso permitido': 'Somente acompanhado')
+        return 'Acesso autorizado somente com acompanhante'        
+    }else if (n1 = 1.39 && n1 >= 1.20){
+        console.log(n2 == 'nao acompanhado'? 'Somente acompanhado': 'Acesso permitido')        
+    }else if(n1 < 1.20){       
+       return 'Acesso negado'
     }
-
-}
-
-console.log(podesubir(1.20 , 'e'))
+        
+    }
+    
+    console.log(podesubir(1.30, 'nao acompanhado'))
+    console.log(podesubir(1.30, 'acompanhado'))
+    console.log(podesubir(1.50, 'nao acompanhado'))
+    console.log(podesubir(1.50, 'acompanhado'))
+    console.log(podesubir(1.19, 'acompanhado'))
+    console.log(podesubir(1.19, 'nao acompanhado'))
+    
