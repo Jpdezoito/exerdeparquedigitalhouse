d3d596ced3a650bde244b7635e8e776d3b6d9869
Jpdezoito <jpdezoito@gmail.com>
2022-11-07 09:55:31 -0300

    modifi

--- exercipodesubir/podesubir.js
+++ exercipodesubir/podesubir.js
@@ -5,12 +5,9 @@ function podesubir(n1, n2) {
     if (n1 >= 1.40 && n1 <= 2.00) {                           
         return 'Acesso autorizado';
     }else if (n1 < 1.40 && n1 > 1.20, Fal ) { 
-        console.log('Acesso autorizado somente com acompanhante')                       
-       
+        return 'Acesso autorizado somente com acompanhante'      
     }
-    else if (n1 < 1.40 && n1 >= 1.20 ) {                    
-        return 'Acesso autorizado'
-    }
+   
     else if (n1 < 1.20) {
         return 'Acesso negado'
     }
