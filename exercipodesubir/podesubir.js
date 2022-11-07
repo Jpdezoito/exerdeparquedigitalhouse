516e18bc2766f2556d4ef699dcefde70eb1f0d16
Jpdezoito <jpdezoito@gmail.com>
2022-11-07 13:47:44 -0300

    modifi

--- exercipodesubir/podesubir.js
+++ exercipodesubir/podesubir.js
@@ -1,10 +1,13 @@
 
 function podesubir(n1, n2) {
-    let acompanhado = (n2 == 'acompanhado'? 'true': 'false')
-    let naoacompanhado = (n2 == 'nao acompanhado'? 'false': 'true')    
+    let boolean = (n2 == 'acompanhado'? 'true': 'false')
+    let acompanhado = (n2 == 'acompanhado'? 'Acompanhado': 'Não acompanhado')
+    let naoacompanhado = (n2 == 'nao acompanhado'? 'Não acompanhado': 'Acompanhado')
+    console.log(boolean)    
     console.log(acompanhado)
     console.log(naoacompanhado) 
      if (n1 >= 1.40 && n1 <= 2.00) {
+        console.log('Maior ou igual 1.40m e menor ou igual a 2.00m')
         return 'Acesso autorizado'
      }else if (n1 = 1.39 && n1 >= 1.20){
         console.log(n2 == 'acompanhado'? 'Acesso permitido': 'Somente acompanhado')
